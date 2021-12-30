7a41d67d1759b3e9c4c883e959a75d7a9e4cdffc
İrem Zeynep Alagöz <irem.zeynep.alagoz@gmail.com>
2021-12-30 13:32:31 +0300

    Update generator inputs, T and logging data

--- resize.py
+++ resize.py
@@ -8,16 +8,22 @@ import os
 import glob
 
 current_path = os.path.dirname(__file__)
-resized_path = os.path.join(current_path, 'resized_data')
-dirs = glob.glob(os.path.join(current_path, 'raw_data/*'))
-files = [ glob.glob(dir+'/*') for dir in dirs ]
-files = sum(files, []) # flatten
+
+resized_path = os.path.join('/media/tetam/One Touch/2232 experiments/alternativeMocogan/mocogan', 'resized_data/')
+print(resized_path)
+p = "/media/tetam/One Touch/2232 experiments/Grouped By Distance/40/*"
+
+files = glob.glob(os.path.normpath(p))
+
+
 
 ''' script for cropping '''
 for i, file in enumerate(files):
-    os.system("ffmpeg -i %s -pix_fmt yuv420p -vf crop=96:96:42:24 %s.mp4" %
-             (file, os.path.join(resized_path, str(i))))
+    os.system("ffmpeg -i '%s' -pix_fmt yuv420p -ss 00:00:20 -t 00:00:45 -vf format=gray,crop=512:512:0:219,scale=96:96,eq=contrast=1.5,fps=10 '%s.mp4'" %
+            (file, os.path.join(resized_path, str(i))))
 
+
+#os.system
 ''' script for reducing size '''
 # # resize to 96x76
 # for i, file in enumerate(files):
