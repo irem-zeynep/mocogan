0ebf2fd613726f96acf8af716eca8ddfa2886329
Kadir Elmacı <abdulkadirelmaci@gmail.com>
2022-01-05 23:17:32 +0300

    Fix runner logs

--- runner.py
+++ runner.py
@@ -13,14 +13,16 @@ pre_train  = args.pre_train
 
 while True:
 	start_time = time.time()
+	numberFile = open("lastTrainedNumber.txt", "r")
+	iterationNumber = numberFile.readline()
+	numberFile.close()
 	if pre_train:
-		numberFile = open("lastTrainedNumber.txt", "r")
-		iterationNumber = numberFile.readline()
 		os.system(("python3 train.py --niter 10000 --pre-train {}").format(iterationNumber))
-		numberFile.close()
 	else:
+		iterationNumber = 0
 		os.system("python3 train.py --niter 10000")
-	logFile.write("Start time: {}, lastTrainNumber: {}\n".format(start_time, iterationNumber))
+	
+	logFile.write(("Start time: {}, lastTrainNumber: {}\n").format(start_time, iterationNumber))
 	logFile.flush()
 	
 logFile.close()
